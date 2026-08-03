0907675c35d13d1dc682172fc13ab0c61a314d15
Nasirah Mohamud <nasirahmohamud@gmail.com>
2026-08-03 08:03:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ const observer = new IntersectionObserver((entries) => {
 sections.forEach((section) => {
     observer.observe(section);
 });
+const hiddenElements = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+hiddenElements.forEach(section=>{
+
+    section.classList.add("hidden");
+
+    observer.observe(section);
+
+});
